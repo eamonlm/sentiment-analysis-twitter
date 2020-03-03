bfec37e2364389b53f41c3b5c7b52914697f18d9
eamonlm <elopez7@nd.edu>
2020-03-02 22:20:33 -0500

    Implemented very basics of sentiment analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,10 +1,37 @@
 # sentiment_analysis.py
 '''Given a tweet, this program will look for keywords and identify the sentiment of the tweet towards a given keyword (specifically, candidates for the Democractic nomination)'''
 
-# TODO: find python libraries that deal with sentiment analysis
+from textblob import TextBlob
+from collections import defaultdict
 
-def sentiment_tweet(tweet, keywords):
-    # TODO: return sentiment of tweet towards keywords
-    pass
+''' Dictionary that contains key, value pairs in the following format:
+    Key: Candidate Last Name
+    Value: List with two elements: overall sentiment and num_tweets '''
+        
+# List of names to filter only relevant tweets
+candidate_names = ["sanders", "warren", "buttigieg", "biden", "steyer", "klobuchar", "gabbard"]
 
-# TODO: Pass in a data structure (array?) of tweets. Run sentiment analysis on tweets. Return overall sentiment towards candidates
+# Initializing Dict
+candidates = defaultdict(list)
+for candidate in candidate_names:
+    candidates[candidate] = [0, 0]
+
+# Given a tweet, this returns the sentiment of the tweet
+def sentiment_tweet(tweet):
+    sentiment = TextBlob(tweet).sentiment.polarity
+    return sentiment
+
+# TODO: Implement an actual algorithm
+
+if __name__ == "__main__":
+    # Gets sentiment of all relevant tweets, and adds it to the dictionary
+    for tweet in open("Tweets.txt"):
+        for candidate in candidate_names:
+            if candidate in tweet.lower():
+                overall_sentiment = candidates[candidate][0]
+                num_tweets = candidates[candidate][1]
+                candidates[candidate] = [overall_sentiment + sentiment_tweet(tweet), num_tweets + 1]
+
+    # Display data
+    for candidate in candidates:
+        print(candidate, candidates[candidate], candidates[candidate][0] / candidates[candidate][1])
