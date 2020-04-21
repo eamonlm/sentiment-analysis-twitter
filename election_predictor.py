1f1716b777d7996ea3d054106e59dff3717eb1cd
Lauren Bakke <laurenbakke@Laurens-MBP.hsd1.mn.comcast.net>
2020-04-21 12:16:56 -0500

    polling data for election predictor

---
+++ election_predictor.py
@@ -0,0 +1,23 @@
+# main algorithm 
+
+
+
+# polling data takes the form "trump percentage", "biden percentage" (doesn't add up to 100 because of the "other" category) 
+
+# this data comes from https://www.270towin.com/2020-polls-biden-trump/
+
+pd = {}
+pd['MI'] = [.42, .47]
+pd['NH'] = [.46, .44]
+pd['PA'] = [.47, .45]
+pd['WI'] = [.46, .47]
+
+pd['FL'] = [.44, .47]
+pd['MN'] = [.38, .50]
+pd['NV'] = [.39, .47]
+
+pd['ME'] = [.42, .52]
+pd['AZ'] = [.43, .52]
+pd['NC'] = [.48, .45]
+pd['CO'] = [.45, .55]
+
